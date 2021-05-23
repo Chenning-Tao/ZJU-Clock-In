4775b20485be7ab25fd9980d12536479bd500e1a
cftcn <58070700+cftcn@users.noreply.github.com>
2021-05-23 11:05:15 +0800

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
-
-# 打卡脚修改自ZJU-nCov-Hitcarder的开源代码，感谢这位同学开源的代码
-
-import requests
-import json
-import re
-import datetime
-import time
-import sys
+import requests, json, re
+import time, datetime, os
+import getpass
+from halo import Halo
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-class DaKa(object):
-    """Hit card class
-
+class HitCarder(object):
+    """Hit carder class
     Attributes:
         username: (str) 浙大统一认证平台用户名（一般为学号）
         password: (str) 浙大统一认证平台密码
         login_url: (str) 登录url
         base_url: (str) 打卡首页url
         save_url: (str) 提交打卡url
+        self.headers: (dir) 请求头
         sess: (requests.Session) 统一的session
     """
 
@@ -28,15 +24,16 @@ class DaKa(object):
         self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
         self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
         self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+        }
         self.sess = requests.Session()
 
     def login(self):
-        """Login to ZJU platform"""
-        res = self.sess.get(self.login_url)
-        execution = re.search(
-            'name="execution" value="(.*?)"', res.text).group(1)
-        res = self.sess.get(
-            url='https://zjuam.zju.edu.cn/cas/v2/getPubKey').json()
+        """Login to ZJU platform."""
+        res = self.sess.get(self.login_url, headers=self.headers)
+        execution = re.search('name="execution" value="(.*?)"', res.text).group(1)
+        res = self.sess.get(url='https://zjuam.zju.edu.cn/cas/v2/getPubKey', headers=self.headers).json()
         n, e = res['modulus'], res['exponent']
         encrypt_password = self._rsa_encrypt(self.password, e, n)
 
@@ -46,7 +43,7 @@ class DaKa(object):
             'execution': execution,
             '_eventId': 'submit'
         }
-        res = self.sess.post(url=self.login_url, data=data)
+        res = self.sess.post(url=self.login_url, data=data, headers=self.headers)
 
         # check if login successfully
         if '统一身份认证' in res.content.decode():
@@ -54,19 +51,19 @@ class DaKa(object):
         return self.sess
 
     def post(self):
-        """Post the hitcard info"""
-        res = self.sess.post(self.save_url, data=self.info)
+        """Post the hit card info."""
+        res = self.sess.post(self.save_url, data=self.info, headers=self.headers)
         return json.loads(res.text)
 
     def get_date(self):
-        """Get current date"""
+        """Get current date."""
         today = datetime.date.today()
         return "%4d%02d%02d" % (today.year, today.month, today.day)
 
     def get_info(self, html=None):
-        """Get hitcard info, which is the old info with updated new time."""
+        """Get hit card info, which is the old info with updated new time."""
         if not html:
-            res = self.sess.get(self.base_url)
+            res = self.sess.get(self.base_url, headers=self.headers)
             html = res.content.decode()
 
         try:
@@ -80,10 +77,10 @@ class DaKa(object):
             new_id = new_info_tmp['id']
             name = re.findall(r'realname: "([^\"]+)",', html)[0]
             number = re.findall(r"number: '([^\']+)',", html)[0]
-        except IndexError:
-            raise RegexMatchError('Relative info not found in html with regex')
-        except json.decoder.JSONDecodeError:
-            raise DecodeError('JSON decode error')
+        except IndexError as err:
+            raise RegexMatchError('Relative info not found in html with regex: ' + str(err))
+        except json.decoder.JSONDecodeError as err:
+            raise DecodeError('JSON decode error: ' + str(err))
 
         new_info = old_info.copy()
         new_info['id'] = new_id
@@ -91,16 +88,12 @@ class DaKa(object):
         new_info['number'] = number
         new_info["date"] = self.get_date()
         new_info["created"] = round(time.time())
-        new_info["address"] = "浙江省杭州市西湖区"
-        new_info["area"] = "浙江省 杭州市 西湖区"
-        new_info["province"] = new_info["area"].split(' ')[0]
-        new_info["city"] = new_info["area"].split(' ')[1]
         # form change
         new_info['jrdqtlqk[]'] = 0
         new_info['jrdqjcqk[]'] = 0
-        new_info['sfsqhzjkk'] = 1   # 是否申领杭州健康码
-        new_info['sqhzjkkys'] = 1   # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
-        new_info['sfqrxxss'] = 1    # 是否确认信息属实
+        new_info['sfsqhzjkk'] = 1  # 是否申领杭州健康码
+        new_info['sqhzjkkys'] = 1  # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
+        new_info['sfqrxxss'] = 1  # 是否确认信息属实
         new_info['jcqzrq'] = ""
         new_info['gwszdd'] = ""
         new_info['szgjcs'] = ""
@@ -116,7 +109,7 @@ class DaKa(object):
         return hex(result_int)[2:].rjust(128, '0')
 
 
-# Exceptions
+# Exceptions 
 class LoginError(Exception):
     """Login Exception"""
     pass
@@ -134,49 +127,67 @@ class DecodeError(Exception):
 
 def main(username, password):
     """Hit card process
-
     Arguments:
         username: (str) 浙大统一认证平台用户名（一般为学号）
         password: (str) 浙大统一认证平台密码
     """
-    print("\n[Time] %s" %
-          datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print("\n[Time] %s" % datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     print("🚌 打卡任务启动")
+    spinner = Halo(text='Loading', spinner='dots')
+    spinner.start('正在新建打卡实例...')
+    hit_carder = HitCarder(username, password)
+    spinner.succeed('已新建打卡实例')
 
-    dk = DaKa(username, password)
-
-    print("登录到浙大统一身份认证平台...")
+    spinner.start(text='登录到浙大统一身份认证平台...')
     try:
-        dk.login()
-        print("已登录到浙大统一身份认证平台")
+        hit_carder.login()
+        spinner.succeed('已登录到浙大统一身份认证平台')
     except Exception as err:
-        print(str(err))
-        raise Exception
+        spinner.fail(str(err))
+        return
 
-    print('正在获取个人信息...')
+    spinner.start(text='正在获取个人信息...')
     try:
-        dk.get_info()
-        print('%s %s同学, 你好~' % (dk.info['number'], dk.info['name']))
+        hit_carder.get_info()
+        spinner.succeed('%s %s同学, 你好~' % (hit_carder.info['number'], hit_carder.info['name']))
     except Exception as err:
-        print('获取信息失败，请手动打卡，更多信息: ' + str(err))
-        raise Exception
+        spinner.fail('获取信息失败，请手动打卡，更多信息: ' + str(err))
+        return
 
-    print('正在为您打卡打卡打卡')
+    spinner.start(text='正在为您打卡...')
     try:
-        res = dk.post()
+        res = hit_carder.post()
         if str(res['e']) == '0':
-            print('已为您打卡成功！')
+            spinner.stop_and_persist(symbol='🦄 '.encode('utf-8'), text='已为您打卡成功！')
         else:
-            print(res['m'])
-    except Exception:
-        print('数据提交失败')
-        raise Exception
+            spinner.stop_and_persist(symbol='🦄 '.encode('utf-8'), text=res['m'])
+    except Exception as err:
+        spinner.fail('数据提交失败 ' + str(err))
+        return
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
+    if os.path.exists('./config.json'):
+        configs = json.loads(open('./config.json', 'r').read())
+        username = configs["username"]
+        password = configs["password"]
+        hour = configs["schedule"]["hour"]
+        minute = configs["schedule"]["minute"]
+    else:
+        username = input("👤 浙大统一认证用户名: ")
+        password = getpass.getpass('🔑 浙大统一认证密码: ')
+        print("⏲  请输入定时时间（默认每天6:05）")
+        hour = input("\thour: ") or 6
+        minute = input("\tminute: ") or 5
+    main(username, password)
+
+    # Schedule task
+    scheduler = BlockingScheduler()
+    scheduler.add_job(main, 'cron', args=[username, password], hour=hour, minute=minute)
+    print('⏰ 已启动定时程序，每天 %02d:%02d 为您打卡' % (int(hour), int(minute)))
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
     try:
-        main(username, password)
-    except Exception:
-        exit(1)
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
